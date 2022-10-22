9136ec12f1f158ccf05b18176579df866cb329cc
Rakib Khan <khanrakib218@gmail.com>
2022-10-22 18:16:43 +0600

    Add files via upload

---
+++ flutter_image/lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Image picker';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: const Image(
+              image: NetworkImage(
+                  'https://prnt.sc/VGBLEDkdnkLd'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
